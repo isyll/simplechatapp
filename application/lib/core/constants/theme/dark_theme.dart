8d28b81b1c145c89712851381333fd616f102159
Ibrahima Sylla <isyll711@gmail.com>
2024-09-27 22:04:16 +0000

    Revert "move theme folder"
    This reverts commit 8029a67e3e3071fea712462fcf5f0408958263a2.

--- application/lib/core/constants/theme/dark_theme.dart
+++ application/lib/core/constants/theme/dark_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:simple_chat_app/core/theme/colors.dart';
+import 'package:simple_chat_app/core/constants/theme/colors.dart';
 
-final lightTheme = ThemeData(
+final darkTheme = ThemeData(
     brightness: Brightness.light,
     useMaterial3: true,
     colorScheme: AppColors.lightColorScheme,
--- application/lib/core/constants/theme/light_theme.dart
+++ application/lib/core/constants/theme/light_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:simple_chat_app/core/theme/colors.dart';
+import 'package:simple_chat_app/core/constants/theme/colors.dart';
 
-final darkTheme = ThemeData(
+final lightTheme = ThemeData(
     brightness: Brightness.light,
     useMaterial3: true,
     colorScheme: AppColors.lightColorScheme,
